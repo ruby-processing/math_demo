6092d97a414fe01579d7c03559c10b2454a4f07b
monkstone <mamba2928@yahoo.co.uk>
2016-12-15 07:35:28 +0000

    proper bisector test

--- test/pbisector_test.rb
+++ test/pbisector_test.rb
@@ -5,13 +5,21 @@ METHODS = [:midpoint, :angle]
 
 class PBisectorTest < Minitest::Test
   def setup
-    @bisector = PBisector.new(Vec2D.new(1, 2), Vec2D.new(50, 100))
+    @bisector = PBisector.new(Vec2D.new(100, 50), Vec2D.new(500, 350))
   end
 
   def test_respond
     METHODS.each do |method_string|
       assert_respond_to @bisector, method_string
     end
+  end
+
+  def test_midpoint
+    assert_equal @bisector.midpoint, Vec2D.new(300, 200)
+  end
+
+  def test_angle # Angle of bisector to the 3, 4, 5 triangle
+    assert_in_delta @bisector.angle, 36.87.radians - Math::PI / 2
   end
 
   def tear_down
